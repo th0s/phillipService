7db0325668fc2c55118a736d575ece7b8d51cbdd
Phillip Schmidt <markidex@gmail.com>
2018-06-20 20:59:50 -0700

    Seeding successfully 3.5 mmillion per hour

--- db/dbConnect.js
+++ db/dbConnect.js
@@ -1,7 +1,13 @@
 
 const cassandra = require('cassandra-driver');
 
-const client = new cassandra.Client({contactPoints: ['127.0.0.1'], keyspace: 'ad_service'});
+const client = new cassandra.Client({
+  contactPoints: ['127.0.0.1'], 
+  keyspace: 'ad_service',
+  protocalOptions: {
+    port: 30041
+  }
+});
 
 client.connect((err, result) => {
   if (err) { console.log('There was an error: ', err)}
--- db/helpers/seedScript.js
+++ db/helpers/seedScript.js
@@ -2,38 +2,60 @@ const client = require('../dbConnect');
 const faker = require('faker');
 
 
-const seed = () => {
-  let id = faker.random.number(10000000)
-  let name = faker.lorem.words()
-  let description = faker.lorem.sentence()
-  let duration = faker.random.number(500)
-  let url = faker.internet.url()
-  let tag = faker.lorem.word()
+const seed = (count) => { 
 
-  let insert = `
-    INSERT INTO ads (
-      id, name, description, duration, url, tags
-    ) 
+  for (let i = 0; i < count; i++) {
+    let id = faker.random.number(10000000)
+    let name = faker.lorem.words()
+    let description = faker.lorem.sentence()
+    let duration = faker.random.number(500)
+    let url = faker.internet.url()
+    let tag = faker.lorem.word()
+
+    let insert = `
+    INSERT INTO ads (id, name, description, duration, url, tag) 
     VALUES (
-      ${id},
-      ${name},
-      ${description},
-      ${duration},
-      ${url},
-      ${tag}
-    )
-  `
-  client.execute(insert, (err, result) => {
-    if (err) { 
-      console.log('There was an insertion err: ', err)
-      return
+      ${id}, 
+      '${name}', 
+      '${description}',
+      ${duration}, 
+      '${url}', 
+      '${tag}'
+    )`;
+    
+    console.time('Real Insert Time is : ');
+    client.execute(insert, (err, result) => {
+      if (err) { 
+        console.log('There was an insertion err: ', err)
+        return
+      }
+    })
+    console.timeEnd('Real Insert Time is : ');
+  }
+  console.log(count)  
+  return
+}
+
+const timer = () => {
+  let final = 60340;
+
+  setInterval(() => {
+    if (final === 10000000) {
+      console.log('Finshed!');
+      return;
     }
-    console.log(result, '<------ is result')
-  })
+    
+    let counter = 2;
+    for (let i = 0; i < counter; i++) {
+      seed(1000);
+    }
+    final += 2000;
+  }, 2000);
+
   
 }
-console.log(seed())
+timer();
 
 module.exports = {
  seed
-};
+};    
