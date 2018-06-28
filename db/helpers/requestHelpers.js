599391d50ac24cec4a90fa6c9bd9ed7bd5599112
Phillip Schmidt <markidex@gmail.com>
2018-06-27 20:52:26 -0700

    Added 3 helpers to requestHelpers.

--- db/helpers/requestHelpers.js
+++ db/helpers/requestHelpers.js
@@ -1,8 +1,12 @@
-const axios = require('axios'); 
+// Libraries
+const axios = require('axios');
+
+// Files
+const dbHelper = require('../helpers/dbHelpers');
 
 // Request User Profile from User Service
 const getUserProfile = (userId, tag, cb) => {
-  console.log(userId)
+
   axios.get('http://userservice/user/:userId', {
     userId: userId,
     tag: tag
@@ -15,6 +19,60 @@ const getUserProfile = (userId, tag, cb) => {
     cb(null, res)
   })
 };
+
+const compileTags = (userProfile, newTags, cb) => {
+  let userTagsArr = userProfile.tags;
+  let tagCompare = newTags[Math.floor(Math.random() * newTags.length)];
+  // If the userTagsArr has any of the tags from the newly queued video tags
+  // Pull ad with that tag.
+  if (userTagsArr.includes(tagCompare)) {
+    let pullTag = tagCompare;
+    let newAd = dbHelper.findAd(pullTag, (err, result) => {
+      if (err) (cb(err, null));
+      cb(null, result);
+    })
+
+  } else {
+    // If it doesn't contain that tag, chose random tag to pull with from currenly queued video tag list.
+    let newAd = dbHelper.findAd(userTagsArr[Math.floor(Math.random() * userTagsArr.length)], (err, result) => {
+      if (err) {
+        cb(err, null)
+      }
+      cb(null, err)
+    })
+  }
+};
+
+const runSystem = (userId, tags, cb) => {
+  getUserProfile(userId, tags, (err, result) => {
+
+    if (err) {
+      console.log('There was an error getting User Profile.')
+      cb(err, null)
+    }
+
+    compileTags(result, tags, (err, result) => {
+
+    })
+
+  })
+
+}
+
+const userProfile = {
+  username:  'test',
+  subscriptions: [],
+  tags: ['trending', 'gaming'],
+  country:   'USA',
+  state: 'OR',
+  city: 'Troutdale',
+  dob: 3172018,
+  email: 'test@yahoo.com'
+};
+
+const newTags = ['insurance', 'gaming'];
+
+compileTags(userProfile, newTags);
 
 module.exports = {
   getUserProfile
