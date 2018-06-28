1b65a793a7fe654758ecdc77161aecf553a07d8c
Phillip Schmidt <markidex@gmail.com>
2018-06-27 19:13:35 -0700

    Added tag to getUserProfile helper

--- db/helpers/requestHelpers.js
+++ db/helpers/requestHelpers.js
@@ -1,10 +1,11 @@
 const axios = require('axios'); 
 
 // Request User Profile from User Service
-const getUserProfile = (userId, cb) => {
+const getUserProfile = (userId, tag, cb) => {
   console.log(userId)
   axios.get('http://userservice/user/:userId', {
-    userId: userId
+    userId: userId,
+    tag: tag
   })
   .then((err, res) => {
     if (err) { 
